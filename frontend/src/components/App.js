75c32d87abf038c7b29b62db5812348d852b7d13
Edwin Polycarpe <edpolyca@amazon.com>
2022-04-08 16:53:22 +0200

    Basic react app

---
+++ frontend/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../assets/logo.png'
+
+function App() {
+  return (
+    <div className="App"> 
+      <img className="logo" src={logo} alt="app-logo" />
+      <h3>Welcome to pychain</h3>
+    </div>
+  );
+}
+
+export default App;
